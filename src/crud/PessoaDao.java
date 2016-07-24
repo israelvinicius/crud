a5e9b3820c9e7b933f110809ce6bac38450974ab
Israel <israelvinicius14@hotmail.com>
2016-07-24 18:16:50 -0300

    commit inicial

---
+++ src/crud/PessoaDao.java
@@ -0,0 +1,20 @@
+package crud;
+
+public class PessoaDao {
+
+	public void salvar(Pessoa pessoa) {
+
+	}
+
+	public void editar(Pessoa pessoa) {
+
+	}
+
+	public Pessoa[] listar() {
+		return null;
+	}
+
+	public void deletar(Pessoa pessoa) {
+
+	}
+}
---
+++ src/crud/Start.java
@@ -0,0 +1,10 @@
+package crud;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
